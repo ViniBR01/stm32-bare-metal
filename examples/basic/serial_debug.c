f6e6fd57916ea20e377d0d63a048eb4bc394a98b
Vini Da Silva <viniciusgoncalves_29@hotmail.com>
2025-10-02 18:13:15 -0700

    Refactor serial_debug.c to improve LED control timing and initialization sequence

--- examples/basic/serial_debug.c
+++ examples/basic/serial_debug.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
+#include "led2.h"
 #include "uart_terminal.h"
 #include "systick.h"
 
 int main(void) {
     uart_terminal_init();
-
+    led2_init();
     printf("Hello, UART Terminal!\n");
 
     while (1) {
-        systick_delay_ms(1000);
+        led2_off();
+        systick_delay_ms(200);
+        led2_on();
+        systick_delay_ms(800);
         printf("Tick...\n");
     }
 }
