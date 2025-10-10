0b031456a949a266d8e86a7384c03c7e7d0c37e5
Vini Da Silva <viniciusgoncalves_29@hotmail.com>
2025-10-09 20:34:45 -0700

    Uses gpio_handler to refactor the user_button driver.

--- drivers/inc/user_button.h
+++ drivers/inc/user_button.h
@@ -1,9 +1,9 @@
 #ifndef USER_BUTTON_H_
 #define USER_BUTTON_H_
 
-#include "stm32f4xx.h"
+#include <stdint.h>
 
 void user_button_init(void);
-int user_button_get_state(void);
+uint8_t user_button_get_state(void);
 
 #endif /* USER_BUTTON_H_ */
