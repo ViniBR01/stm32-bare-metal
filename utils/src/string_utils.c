21dd6f89e83b40e2d70e460b68a6e625d427280b
Vini Da-Silva <viniciusgoncalves_29@hotmail.com>
2025-10-23 22:13:12 -0700

    Suppress -Wnonnull-compare warnings in string_utils.c

--- utils/src/string_utils.c
+++ utils/src/string_utils.c
@@ -1,5 +1,7 @@
 #include "string_utils.h"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnonnull-compare"
 size_t strlen(const char *str) {
     size_t len = 0;
     if (str) {
@@ -9,7 +11,10 @@ size_t strlen(const char *str) {
     }
     return len;
 }
+#pragma GCC diagnostic pop
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnonnull-compare"
 char *strcpy(char *dest, const char *src) {
     char *orig_dest = dest;
     if (dest && src) {
@@ -19,7 +24,10 @@ char *strcpy(char *dest, const char *src) {
     }
     return orig_dest;
 }
+#pragma GCC diagnostic pop
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnonnull-compare"
 int strncmp(const char *s1, const char *s2, size_t n) {
     if (!s1 || !s2 || n == 0) {
         return 0;
@@ -38,6 +46,7 @@ int strncmp(const char *s1, const char *s2, size_t n) {
     }
     return 0;
 }
+#pragma GCC diagnostic pop
 
 void *memcpy(void *dest, const void *src, size_t n) {
     unsigned char *d = (unsigned char *)dest;
